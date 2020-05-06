d607d3d1528f927cc8869a83979a98c55cd23f98
Marius-Juston <Marius.juston@hotmail.fr>
2020-05-05 23:26:30 -0400

    created trapezoid class that uses dict as motion data and has a data point defined

--- motion_planning/motion_planner.py
+++ motion_planning/motion_planner.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
-from typing import Tuple
+from typing import Tuple, Optional, Dict
 
 import numpy as np
+from matplotlib.figure import Figure
 
 
 class MotionProfile(ABC):
     @abstractmethod
-    def get_data(self, t) -> Tuple[float]:
+    def get_data(self) -> Tuple[Tuple[float, float, float, float]]:
         pass
 
 
@@ -15,135 +16,105 @@ class NthOrderSCurve:
 
 
 class TrapezoidalCurve(MotionProfile):
-    def find_accelerating_constants(self, v_initial, v_final, acceleration_max):
+    def find_accelerating_constants(self, v_initial: float, v_final: float, acceleration_max: float) \
+            -> Dict[str, float]:
         delta_v = v_final - v_initial
 
         if delta_v == 0:
-            return 0, 0, v_initial, 0
+            return {"time": 0, "distance": 0, "velocity": v_initial, "acceleration": 0}
 
         accel_sign = np.sign(delta_v)
         a = accel_sign * acceleration_max
         t = delta_v / a
         d = (v_final ** 2 - v_initial ** 2) / (2 * a)
 
-        return t, d, v_initial, a
+        return {"time": t, "distance": d, "velocity": v_initial, "acceleration": a}
 
-    def find_flat_constants(self, s_initial, s_final, d_a, d_b, v_max):
+    def find_flat_constants(self, s_initial, s_final, d_a, d_b, v_max) -> Dict[str, float]:
         delta_d = s_final - s_initial - d_a - d_b
         t = delta_d / v_max
 
-        return t, delta_d, v_max, 0
+        return {"time": t, "distance": delta_d, "velocity": v_max, "acceleration": 0}
 
     def __init__(self, s_initial, s_final, v_initial, v_final, v_max, acceleration_max) -> None:
         super().__init__()
 
-    def get_data(self, t) -> Tuple[float]:
-        pass
+        self.s_initial = s_initial
+
+        accel = self.find_accelerating_constants(v_initial, v_max, acceleration_max)
+        deccel = self.find_accelerating_constants(v_max, v_final, acceleration_max)
+
+        distance_accelerating = accel['distance']
+        distance_decelerating = deccel['distance']
+
+        delta_s = s_final - s_initial
+
+        if delta_s == distance_accelerating + distance_decelerating:
+            self.motion = (accel, deccel)
+        elif abs(distance_decelerating + distance_accelerating) > abs(delta_s):
+            pass
+        else:
+            flat = self.find_flat_constants(s_initial, s_final, distance_accelerating, distance_decelerating, v_max)
+            self.motion = (accel, flat, deccel)
+
+        self.period = sum(m['time'] for m in self.motion)
+
+    def position(self, s, v, a, t):
+        return s + v * t + t ** 2 / 2 * a
+
+    def velocity(self, v, a, t):
+        return v + a * t
+
+    def get_data_point(self, t) -> Optional[Tuple[float, float, float, float]]:
+        if t > self.period:
+            return None
+
+        i = 0
+        t_i = self.motion[i]['time']
+        s = self.s_initial
+
+        while t > t_i:
+            i += 1
+            s += self.motion[i]['distance']
+            t_i += self.motion[i]['time']
+
+        t_i -= self.motion[i]['time']
+        t2 = t- t_i
+
+        v = self.motion[i]['velocity']
+        a = self.motion[i]['acceleration']
+
+        p = self.position(s, v, a, t2)
+        v = self.velocity(v, a, t2)
+
+        return t, p, v, a
+
+    def get_data(self, precision=1000) -> np.ndarray:
+        data = []
+
+        for t in np.linspace(0, self.period, precision):
+            data.append(self.get_data_point(t))
+
+        return np.array(data)
 
 
-# class TrapezoidalCurve:
-#     def find_constants(self, v_initial, v_final, acceleration_max):
-#         delta_v = v_final - v_initial
-#         accel_sign = np.sign(delta_v)
-#
-#         a = accel_sign * acceleration_max
-#         t_a = delta_v / a
-#         d = (v_final ** 2 - v_initial ** 2) / (2 * a)
-#
-#         def motion(s_initial, t):
-#             s = s_initial + v_initial * t + a * t ** 2 / 2
-#             v = v_initial + a * t
-#             return s, v, a
-#
-#         return t_a, d, a, motion
-#
-#     def find_flat(self, s_initial, s_final, d_a, d_b, v_max):
-#         delta = (s_final - s_initial) - d_a - d_b
-#         t_c = delta / v_max
-#
-#         def motion(s_initial, t):
-#             s = s_initial + v_max * t
-#             return s, v_max, 0
-#
-#         return t_c, delta, 0, motion
-#
-#     def __init__(self, s_initial, s_final, v_initial, v_final, v_max, acceleration_max) -> None:
-#         super().__init__()
-#
-#         self.s_initial = s_initial
-#
-#         self.times = []
-#         self.motion = []
-#
-#         t_a, d_a, a_up, m_a = self.find_constants(v_initial, v_max, acceleration_max)
-#         t_b, d_b, a_down, m_b = self.find_constants(v_max, v_final, acceleration_max)
-#
-#         delta_s = s_final - s_initial
-#
-#         if d_a + d_b == delta_s:
-#             self.times = [t_a, t_b]
-#             self.motion = [m_a, m_b]
-#
-#         if abs(d_a + d_b) > abs(delta_s):
-#             self.times = []
-#         else:
-#             t_c, d_c, a_flat, m_c = self.find_flat(s_initial, s_final, d_a, d_b, v_max)
-#             self.times = [t_a, t_c, t_b]
-#             self.motion = [m_a, m_c, m_b]
-#
-#     def draw_motion(self, precision=50):
-#         time = np.linspace(0, np.sum(self.times), precision)
-#
-#         index = 0
-#
-#         fig: Figure = plt.figure()
-#         position, velocity, acceleration = fig.subplots(3, 1, sharex=True)
-#
-#         s_initial = self.s_initial
-#
-#         p_s = []
-#         v_s = []
-#         a_s = []
-#
-#         t_i = 0
-#
-#         stop = False
-#
-#         for t in time:
-#             while (t - t_i) >= self.times[index]:
-#                 t_i += self.times[index]
-#                 index += 1
-#
-#                 if index >= len(self.times):
-#                     stop = True
-#                     break
-#
-#                 s_initial = p_s[-1]
-#
-#             if stop:
-#                 break
-#
-#             p, v, a = self.motion[index](s_initial, t - t_i)
-#             p_s.append(p)
-#             v_s.append(v)
-#             a_s.append(a)
-#
-#         t = time[:len(p_s)]
-#         position.plot(t, p_s)
-#         velocity.plot(t, v_s)
-#         acceleration.plot(t, a_s)
-#
-#         position.grid()
-#         position.minorticks_on()
-#         velocity.grid()
-#         velocity.minorticks_on()
-#         acceleration.grid()
-#         acceleration.minorticks_on()
-#
-#         plt.tight_layout()
-#         plt.show()
-
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    t = TrapezoidalCurve(5, -10, -1, 0, -1.5, 1)
-    t.draw_motion(500)
+    t = TrapezoidalCurve(0, 10, 0, 0, 2, 1)
+
+    d = t.get_data()
+
+    fig: Figure = plt.gcf()
+    p, v, c = fig.subplots(3, 1)
+
+    t = d[:, 0]
+    p_y = d[:, 1]
+    v_y = d[:, 2]
+    a_y = d[:, 3]
+
+    p.plot(t, p_y)
+    v.plot(t, v_y)
+    c.plot(t, a_y)
+
+    plt.show()
